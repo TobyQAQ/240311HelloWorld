c7ae5cfeb8da2ec14d8a9f58052c6a52b5729f8b
TobyQAQ <a0988065874@gmail.com>
2024-03-11 15:06:15 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _240311HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.txtSHOW = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGO.Location = new System.Drawing.Point(600, 151);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(188, 112);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "按鈕";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // txtSHOW
+            // 
+            this.txtSHOW.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtSHOW.Location = new System.Drawing.Point(3, 151);
+            this.txtSHOW.Multiline = true;
+            this.txtSHOW.Name = "txtSHOW";
+            this.txtSHOW.Size = new System.Drawing.Size(584, 112);
+            this.txtSHOW.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSHOW);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.TextBox txtSHOW;
+    }
+}
+
